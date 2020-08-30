e92098696c431fb71b5dc900e8281f5ff8a66230
mauriciotoso <mauricio.toso@yahoo.com.ar>
2020-08-30 19:59:10 -0300

    Inicial

--- app/src/main/java/com/damlab/sendmeal/MainActivity.java
+++ app/src/main/java/com/damlab/sendmeal/MainActivity.java
@@ -3,13 +3,23 @@ package com.damlab.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText cajaNumero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola
+
+        cajaNumero= findViewById(R.id.cajaNumero);
+    }
+
+    public void BotonGuardar(View view){
+        Toast.makeText(this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
     }
 }
